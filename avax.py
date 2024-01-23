999313d7104b931a492ab964f4afdc81397f4c7f
ArazAbedini <araz.abedini1381@gmail.com>
2024-01-23 16:24:16 +0330

    new method of find peak created

--- avax.py
+++ avax.py
@@ -31,19 +31,33 @@ def add_date_feature(df: pd.DataFrame) -> pd.DataFrame:
     return df_result
 
 
+def find_actual_peak(smooth_peak: np.ndarray, close: np.ndarray) -> np.ndarray:
+    TOLERANCE = 5
+    output_arr = np.zeros(len(smooth_peak), dtype=np.int32)
+    for i in range(len(smooth_peak)):
+        start = max(smooth_peak[i] - 5, 0)
+        end = min(smooth_peak[i] + 5, len(close))
+        max_index_close = np.argmax(close[start: end])
+        output_arr[i] = max_index_close + start
+    output = np.unique(output_arr)
+    return output
+
 def label_process(close: np.ndarray) -> np.ndarray:
     fig, ax = plt.subplots()
-    smooth_arr = signal.savgol_filter(close, window_length=14, polyorder=2)
+    smooth_arr = signal.savgol_filter(close, window_length=9, polyorder=3)
     smooth_peak, _ = signal.find_peaks(x=smooth_arr)
+    smooth_peak = find_actual_peak(smooth_peak, close)
     main_peak, _ = signal.find_peaks(x=close)
     main_valley, _ = signal.find_peaks(x=-1 * close)
     print(f'the length of main_peak is {len(main_peak)} and the length of main_valley is {len(main_valley)}')
     print(f'length of smooth arr is {len(smooth_peak)}')
     smooth_valley, _ = signal.find_peaks(x=-1 * smooth_peak)
     moving_average = df['close'].rolling(7).mean().shift(-2)
-    ax.plot(moving_average, color='red')
-    ax.plot(close, color='black')
-    ax.plot(smooth_arr, color='blue')
+    ax.plot(close[smooth_peak], color='red')
+    ax.plot(close, color='blue')
+    # ax.plot(moving_average, color='red')
+    # ax.plot(close, color='black')
+    # ax.plot(smooth_arr, color='blue')
 
 
     plt.show()
@@ -61,6 +75,7 @@ if __name__ == '__main__':
     df = pd.DataFrame(content_list)
     df = add_date_feature(df)
     X = df.drop(['id', 'open_time', 'time_frame', 'exchange', 'symbol'], axis='columns')[:-1]
+    print(X)
     columns_list = ['open', 'close', 'high', 'low', 'volume']
     df[columns_list] = df[columns_list].astype(np.float32)
     close_array = np.array(df['close'])
